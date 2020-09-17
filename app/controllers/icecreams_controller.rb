ecf230d4863d9b209c91b3993389b27aa029324e
Elliott <elliottcjardin@gmail.com>
2020-09-17 13:37:32 -0400

    add icecreams

--- app/controllers/icecreams_controller.rb
+++ app/controllers/icecreams_controller.rb
@@ -8,4 +8,14 @@ class IcecreamsController < ApplicationController
 
   end
 
+  def new
+
+  end
+
+  def create
+    @icecream = IceCream.new(params.require(:icecream).permit(:ice_cream, :flavor, :presentation, :color_appearance, :aroma, :texture))
+    @icecream.save
+    redirect_to icecreams_path(@icecream)
+  end
+
 end
