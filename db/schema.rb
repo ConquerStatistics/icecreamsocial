6b2c0a0bd90cfdc6a36b7f7a050221a16b08474c
Elliott <elliottcjardin@gmail.com>
2020-10-01 08:05:52 -0400

    Add users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_21_235142) do
+ActiveRecord::Schema.define(version: 2020_10_01_115712) do
 
   create_table "articles", force: :cascade do |t|
     t.string "iceCream"
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 2020_09_21_235142) do
     t.float "texture"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
