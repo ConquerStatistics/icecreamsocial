a5d7eb238ba89b7a3d4e536cd61d619d00289089
Elliott <elliottcjardin@gmail.com>
2020-10-12 22:12:41 -0400

    add first draft

---
+++ app/controllers/shoutouts_controller.rb
@@ -0,0 +1,31 @@
+class ShoutoutsController < ApplicationController
+  def s1
+  end
+
+  def s2
+  end
+
+  def s3
+  end
+
+  def s4
+  end
+
+  def s5
+  end
+
+  def s6
+  end
+
+  def s7
+  end
+
+  def s8
+  end
+
+  def s9
+  end
+
+  def s10
+  end
+end
---
+++ test/controllers/shoutouts_controller_test.rb
@@ -0,0 +1,54 @@
+require 'test_helper'
+
+class ShoutoutsControllerTest < ActionDispatch::IntegrationTest
+  test "should get s1" do
+    get shoutouts_s1_url
+    assert_response :success
+  end
+
+  test "should get s2" do
+    get shoutouts_s2_url
+    assert_response :success
+  end
+
+  test "should get s3" do
+    get shoutouts_s3_url
+    assert_response :success
+  end
+
+  test "should get s4" do
+    get shoutouts_s4_url
+    assert_response :success
+  end
+
+  test "should get s5" do
+    get shoutouts_s5_url
+    assert_response :success
+  end
+
+  test "should get s6" do
+    get shoutouts_s6_url
+    assert_response :success
+  end
+
+  test "should get s7" do
+    get shoutouts_s7_url
+    assert_response :success
+  end
+
+  test "should get s8" do
+    get shoutouts_s8_url
+    assert_response :success
+  end
+
+  test "should get s9" do
+    get shoutouts_s9_url
+    assert_response :success
+  end
+
+  test "should get s10" do
+    get shoutouts_s10_url
+    assert_response :success
+  end
+
+end
